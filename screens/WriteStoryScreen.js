71375149aae70d3a390cc4a85b16d541008d79a2
srushteeM <59560681+srushteeM@users.noreply.github.com>
2020-11-20 16:42:58 +0530

    Update WriteStoryScreen.js

--- screens/WriteStoryScreen.js
+++ screens/WriteStoryScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity,
+    KeyboardAvoidingView,ToastAndroid} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import {Header} from 'react-native-elements';
-
-import firebase from 'firebase'
 import db from '../config'
+//import firebase from 'firebase'
 
 export default class WriteStoryScreen extends React.Component {
     constructor(props){
@@ -15,6 +15,7 @@ export default class WriteStoryScreen extends React.Component {
             storyText: '',
         }
     }
+
     submitStory = ()=>{
         db.collection("stories").add({
             title: this.state.title,
@@ -27,15 +28,19 @@ export default class WriteStoryScreen extends React.Component {
             author: '',
             storyText: ''
         })
+        ToastAndroid.show('Your story has been sumitted'
+        , ToastAndroid.SHORT)
     }
+
     render(){
         return(
-            <View style={styles.container}>
-                <Header 
-                    backgroundColor = {'purple'}
-                    centerComponent = {{
-                        text : 'Bed Time Stories',
-                        style : { color: 'white', fontSize: 22}
+            <KeyboardAvoidingView style={styles.container}
+             behavior="padding" enabled>
+               <Header 
+                    backgroundColor = {'pink'}
+                     centerComponent = {{
+                        text : 'Story Hub',
+                        style : { color: 'black', fontSize: 30}
                     }}
                 />
                 <TextInput 
@@ -45,6 +50,7 @@ export default class WriteStoryScreen extends React.Component {
                             title: text
                         })
                     }}
+                    placeholderTextColor='black'
                     value={this.state.title}
                     style={styles.title}/>
                 <TextInput
@@ -54,6 +60,7 @@ export default class WriteStoryScreen extends React.Component {
                             author: text
                         })
                     }}
+                    placeholderTextColor='black'
                     value={this.state.author}
                     style={styles.author} />
                 <TextInput 
@@ -63,16 +70,18 @@ export default class WriteStoryScreen extends React.Component {
                             storyText: text
                         })
                     }}
+                    placeholderTextColor='black'
                     value={this.state.storyText}
                     style={styles.storyText}
                     multiline={true}/>
                 
                 <TouchableOpacity
                     style={styles.submitButton}
-                    onPress={this.submitStory}>
+                    onPress={this.submitStory}
+                    >
                     <Text style={styles.buttonText}>Submit</Text>
                 </TouchableOpacity>
-            </View>
+            </KeyboardAvoidingView>
         );
     }
 }
@@ -86,29 +95,35 @@ const styles = StyleSheet.create({
       height: 40,
       borderWidth: 2,
       marginTop: 40,
-      margin: 10
+      margin: 10,
+      color:'black',
+      padding: 6,
+
   },
   author: {
       height: 40,
       borderWidth: 2,
-      margin: 10
+      margin: 10,
+       padding: 6,
   },
   storyText: {
       height: 250,
       borderWidth: 2,
-      margin: 10
+      margin: 10, 
+      padding: 6,
+      textAlignVertical: "top"
   },
   submitButton:{
       justifyContent: 'center',
       alignSelf: 'center',
-      backgroundColor: 'blue',
-      width: 100,
-      height: 40,
-      borderRadius:10
+      backgroundColor: 'pink',
+      width: 80,
+      height: 40,color:'black',
   },
   buttonText: {
       textAlign: 'center',
       color: 'white',
-      fontWeight: 'bold'
+      fontWeight: 'bold',
+      color:'black',
   }
 });
